c871e6fded5eace41144e9b7b49c9880a6b65582
lauraTchaly <lauramanes@outlook.com>
2023-03-15 15:54:22 -0300

    CRIANDO PAGINA HOME

--- projetologin/src/app/app-routing.module.ts
+++ projetologin/src/app/app-routing.module.ts
@@ -22,7 +22,11 @@ const routes: Routes = [
 		path: '',
 		redirectTo: '/login',
 		pathMatch: 'full'
-	}
+	},
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  }
 ];
 @NgModule({
 	imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
