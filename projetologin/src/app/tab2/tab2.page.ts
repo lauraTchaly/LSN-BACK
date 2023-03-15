bd38946ec3a88098103ce6ebfbfa9d54aadd4251
lauraTchaly <lauramanes@outlook.com>
2023-03-15 16:35:54 -0300

    CONFIGURANDO PAGE/TAB2

--- projetologin/src/app/tab2/tab2.page.ts
+++ projetologin/src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,15 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private route: Router) {}
+  perfil() {
+    this.route.navigate(['/perfil']);
+  }
+  home() {
+    this.route.navigate(['/inicial']);
+  }
+  atividade() {
+    this.route.navigate(['/atividade']);
+  }
 
 }
